5e72946bef8027e27ec9506f4697095d4fce722b
sarah-codebug <33607713+sarah-codebug@users.noreply.github.com>
2019-07-16 08:53:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){ 
 	// Your code goes below!
+	// HINT!!!!! uncomment out the goodbye hide first!! 
+	// In order to use fadeIn you must first fadeOut or hide it!
+
+	
+	// $("#goodbye").hide()
+
+
 
 
 });
